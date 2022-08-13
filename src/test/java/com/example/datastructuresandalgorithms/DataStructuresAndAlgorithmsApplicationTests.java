aeeee242f105e0e2a3d5baf947e3eca44b415fb6
ChenYong <chenyong@wshifu.com>
2022-08-13 11:55:48 +0800

    add init files

---
+++ src/test/java/com/example/datastructuresandalgorithms/DataStructuresAndAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datastructuresandalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresAndAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
